062d9fa927a62a9c9639cc46b6870130681ec7c8
mao <1296193245@qq.com>
2024-01-03 15:03:12 +0800

    uniapp-device-system

---
+++ pages/index/test5/test5.vue
@@ -0,0 +1,58 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">异步获取系统信息</button>
+		<button type="primary" @click="button2">同步获取系统信息</button>
+		<button type="primary" @click="button3">获取设备基础信息</button>
+		<button type="primary" @click="button4">获取窗口信息</button>
+		<button type="primary" @click="button5">获取APP基础信息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.getSystemInfo({
+					success: (data) => {
+						console.log(data);
+					},
+					fail: (err) => {
+						console.log("获取失败");
+					}
+				})
+			},
+			button2()
+			{
+				const info= uni.getSystemInfoSync();
+				console.log(info);
+			},
+			button3()
+			{
+				const info= uni.getDeviceInfo();
+				console.log(info);
+			},
+			button4()
+			{
+				const info= uni.getWindowInfo();
+				console.log(info);
+			},
+			button5()
+			{
+				const info= uni.getAppBaseInfo();
+				console.log(info);
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+</style>
