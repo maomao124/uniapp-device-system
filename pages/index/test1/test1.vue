db11588ccd75db4cc754436810e8590a65519551
mao <1296193245@qq.com>
2024-01-03 14:19:55 +0800

    uniapp-device-system

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">异步获取系统信息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.getSystemInfo({
+					success: (data) => {
+						console.log(data);
+					},
+					fail: (err) => {
+						console.log("获取失败");
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">异步获取系统信息</button>
+		<button type="primary" @click="button2">同步获取系统信息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.getSystemInfo({
+					success: (data) => {
+						console.log(data);
+					},
+					fail: (err) => {
+						console.log("获取失败");
+					}
+				})
+			},
+			button2()
+			{
+				const info= uni.getSystemInfoSync();
+				console.log(info);
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+</style>
